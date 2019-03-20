42b3f64580a7f2636bc961ab68d4134734ebd066
Gustavolsl <gustavoluizsilvalima0209@gmail.com>
2019-03-20 10:27:48 -0300

    teste

---
+++ Principal.py
@@ -0,0 +1,9 @@
+def somar(x,y):
+    return x + y
+
+def subitrair(x,y):
+    return x - y
+
+def mult(x,y):
+    pass
+
---
+++ test.py
@@ -0,0 +1,8 @@
+import pytest
+from Principal import somar
+#import Principal 
+
+def test_soma():
+    assert somar(2,4)==6
+
+
